cfa08fa114fcdbbfa4b52f5cc297666ad223e9d3
Ilya.Komolkin <ilya.komolkin@jetbrains.com>
2023-04-21 17:13:46 +0300

    UI skeleton finished

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -18,7 +20,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -63,7 +69,7 @@ fun MyApp() {
             })
         }
         composable("results") {
-            ResultsScreen()
+            ResultsScreen(gameViewModel)
         }
     }
 }
@@ -71,7 +77,7 @@ fun MyApp() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PlayerScreen(viewModel: GameViewModel ,onNext: () -> Unit) {
+fun PlayerScreen(viewModel: GameViewModel, onNext: () -> Unit) {
     val players = viewModel.players
 
     Scaffold(
@@ -133,7 +139,6 @@ fun PlayerScreen(viewModel: GameViewModel ,onNext: () -> Unit) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GameScreen(gameViewModel: GameViewModel, onNext: () -> Unit) {
-    val players = gameViewModel.players
     Scaffold(
         topBar = {
             TopAppBar(
@@ -141,23 +146,87 @@ fun GameScreen(gameViewModel: GameViewModel, onNext: () -> Unit) {
             )
         },
         content = { padding ->
-            TextField(
-                //TODO
-                value = players.joinToString(),
-                onValueChange = { },
-                label = { Text("Player Name") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp)
-                    .padding(top = 60.dp),
-            )
+            gameViewModel.run {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 60.dp),
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    gameResults.forEachIndexed { index, result ->
+                        val team1Score = remember { mutableStateOf(result.team1Score) }
+                        val team2Score = remember { mutableStateOf(result.team2Score) }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            val team1Players = gameTeams[index].first
+                            val team2Players = gameTeams[index].second
+                            Text(
+                                text = "${index + 1}.",
+                            )
+                            TextField(
+                                value = team1Score.value.run { this?.toString() ?: "" },
+                                onValueChange = {
+                                    val newValue = it.toIntOrNull() ?: 0
+                                    team1Score.value = newValue
+                                    gameViewModel.setTeam1GameResult(
+                                        index,
+                                        newValue
+                                    )
+                                },
+                                label = { Text(text = "${players[team1Players.first]}/${players[team1Players.second]}") },
+
+                                modifier = Modifier
+                                    .weight(1f),
+                                keyboardOptions = KeyboardOptions.Default.copy(
+                                    keyboardType = KeyboardType.Number
+                                )
+                            )
+                            Text(
+                                text = "vs",
+                            )
+                            TextField(
+                                value = team2Score.value.run { this?.toString() ?: "" },
+                                onValueChange = {
+                                    val newValue = it.toIntOrNull() ?: 0
+                                    team2Score.value = newValue
+                                    gameViewModel.setTeam2GameResult(
+                                        index,
+                                        newValue
+                                    )
+                                },
+                                label = { Text(text = "${players[team2Players.first]}/${players[team2Players.second]}") },
+                                modifier = Modifier.weight(1f),
+                                keyboardOptions = KeyboardOptions.Default.copy(
+                                    keyboardType = KeyboardType.Number
+                                )
+                            )
+
+                        }
+                    }
+                }
+            }
         },
         bottomBar = {
-            Button(
-                onClick = onNext,
-                modifier = Modifier.fillMaxWidth()
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("To Results")
+                if (!gameViewModel.isTourFinished.value) Text(text = "Set down all the games to go to results")
+                Button(
+                    onClick = {
+                        gameViewModel.printGameResults()
+                        onNext()
+                    },
+                    enabled = gameViewModel.isTourFinished.value,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("To results")
+                }
             }
         }
     )
@@ -166,8 +235,7 @@ fun GameScreen(gameViewModel: GameViewModel, onNext: () -> Unit) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ResultsScreen() {
-    // TODO: Implement results screen
+fun ResultsScreen(gameViewModel: GameViewModel) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -175,18 +243,74 @@ fun ResultsScreen() {
             )
         },
         content = { padding ->
-            TextField( //TODO
-                value = "playerName",
-                onValueChange = { },
-                label = { Text("Player Name") },
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp)
-            )
+                    .padding(top = 60.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Player")
+                    Text(text = "-")
+                    Text(text = "Scores")
+                }
+                gameViewModel.players.forEach {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(it)
+                        Text(text = "-")
+                        Text(text = "100")
+                    }
+                }
+            }
         }
     )
 }
 
 class GameViewModel : ViewModel() {
-    val players = mutableStateListOf<String>("1", "2", "3", "4")
+    val players = mutableStateListOf("1", "2", "3", "4", "5")
+    val gameTeams = listOf(
+        Pair(0, 1) to Pair(2, 3),
+        Pair(0, 2) to Pair(3, 4),
+        Pair(0, 3) to Pair(2, 4),
+        Pair(0, 4) to Pair(1, 4),
+    )
+    val gameResults = mutableListOf(
+        GameResult(null, null),
+        GameResult(null, null),
+        GameResult(null, null),
+        GameResult(null, null),
+    )
+
+    val isTourFinished = mutableStateOf(false)
+
+    private fun observeGameResults() {
+        isTourFinished.value = gameResults.all { it.team1Score != null && it.team2Score != null }
+    }
+
+    fun setTeam1GameResult(gameIndex: Int, score: Int) {
+        gameResults[gameIndex].team1Score = score
+        observeGameResults()
+    }
+
+    fun setTeam2GameResult(gameIndex: Int, score: Int) {
+        gameResults[gameIndex].team2Score = score
+        observeGameResults()
+    }
+
+    fun printGameResults() = println(gameResults.map { it.toString() })
+
+}
+
+data class GameResult(var team1Score: Int?, var team2Score: Int?) {
+    override fun toString(): String {
+        return "$team1Score - $team2Score"
+    }
 }
